1a01a803fc46cefebc3bee52f4415eaa2078bd77
PriceisMoney <nrpiyush299@gmail.com>
2025-10-16 18:26:58 +0530

    Add files via upload

---
+++ Perform Basic Mathematical Operations.py
@@ -0,0 +1,15 @@
+#We are performing a basic mathematical Operations using python
+"""we take 2 int value from the user and perform tasks -
+   addition,subtraction,multiplication,division ."""
+#take the value from the user
+
+first_number = int(input('Enter first number:'))
+second_number = int(input('Enter second number:'))
+
+#perform tasks and print it
+
+print('Addition:', first_number + second_number)
+print('Subtraction:', first_number - second_number)
+print('Multiplication:', first_number * second_number)
+print('Division:', first_number / second_number)
+
