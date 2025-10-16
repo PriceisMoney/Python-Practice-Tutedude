bafefa60c48636a2452d8b0488c8bf316fac2a24
Piyush Vishwakarma <nrpiyush299@gmail.com>
2025-10-16 18:55:11 +0530

    Add files via upload

---
+++ Personalized Greeting.py
@@ -0,0 +1,8 @@
+#we are Creating a Personalized Greeting
+'''First we take input from user in str'''
+
+first_name = str(input('Enter first name:'))
+second_name = str(input('Enter second name:'))
+
+#now we will print and put the variable value
+print('Hello', first_name, second_name, '!', 'Welcome to the python program', )
